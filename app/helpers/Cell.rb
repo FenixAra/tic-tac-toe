3f264a5f5f1d39ed11d451dabeda7d7ab0724a7a
FenixAra <aravind_dhan@yahoo.com>
2016-07-06 12:34:49 +0530

    Commenting all files which causes app crash

--- app/helpers/Cell.rb
+++ app/helpers/Cell.rb
@@ -1,19 +1,19 @@
-# Cell.rb : Defines a cell, the basic building block of the Tic-Tac-Toe board
+# # Cell.rb : Defines a cell, the basic building block of the Tic-Tac-Toe board
 
-module TicTacToe
-    class Cell
-        def initialize()
-            @x = 0
-            @y = 0
-            @value = "-"
-        end
+# module TicTacToe
+#     class Cell
+#         def initialize()
+#             @x = 0
+#             @y = 0
+#             @value = "-"
+#         end
         
-        def setCell(value)
-            @value = value
-        end
+#         def setCell(value)
+#             @value = value
+#         end
         
-        def getCell()
-            return @value
-        end
-    end
-end
+#         def getCell()
+#             return @value
+#         end
+#     end
+# end
--- app/helpers/Player.rb
+++ app/helpers/Player.rb
@@ -1,26 +1,26 @@
-# Player.rb : Describes the player properties
+# # Player.rb : Describes the player properties
 
-module TicTacToe
-    class Player
-        def initialize()
-            @name = ""
-            @value = ""
-        end
+# module TicTacToe
+#     class Player
+#         def initialize()
+#             @name = ""
+#             @value = ""
+#         end
         
-        def getName()
-            return @name
-        end
+#         def getName()
+#             return @name
+#         end
         
-        def getValue()
-            return @value
-        end
+#         def getValue()
+#             return @value
+#         end
         
-        def setName(name)
-            @name = name
-        end
+#         def setName(name)
+#             @name = name
+#         end
         
-        def setValue(value)
-            @value = value
-        end
-    end
-end
+#         def setValue(value)
+#             @value = value
+#         end
+#     end
+# end
